5e260ceaa5258be679c24cb9e13005008d71a7a1
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-10 16:15:38 +0545

    Lesson 1 learn to install TS and run it for the first time

---
+++ LESSON_1/index.ts
@@ -0,0 +1,7 @@
+console.log("Hello devs , i'm learning the typescript from today");
+
+let num: number = 5;
+// num = 'string' // its invalid cause the type we declare above is a number
+num = 55
+
+console.log(num);
