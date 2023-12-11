8307f2406360cd3c0ab89ce1941bcb602ae7080e
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-11 12:27:47 +0545

    Learn tsconfig file , wrote sample code , catch errors

--- LESSON_2/index.ts
+++ LESSON_2/index.ts
@@ -16,7 +16,7 @@
 let sums = (a: number, b: number): number => {
     return a + b
 }
-console.log(sums(5, '10')); // valid cause we passed both number as an argument
+console.log(sums(5, 10)); // valid cause we passed both number as an argument
 // sums(1, '2') // invalid cause we passed string as a second parameter
 
 
