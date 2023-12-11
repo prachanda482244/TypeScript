44592fb4b1c0755584f6af4976e0761536061e85
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-11 12:27:26 +0545

    Learn tsconfig file , wrote sample code , catch errors

---
+++ LESSON_1/index.js
@@ -0,0 +1,5 @@
+console.log("Hello devs , i'm learning the typescript from today");
+var num = 5;
+// num = 'string' // its invalid cause the type we declare above is a number
+num = 55;
+console.log(num);
---
+++ LESSON_2/index.ts
@@ -0,0 +1,22 @@
+// Todo :1 Write a program to add two numbers.
+// Todo :2 How to catch erros and solve it
+// Todo :3 TS configuration file
+
+// This is how we can do it in normal js
+
+// Fun define
+// let sum = (a,b)=>{
+// return a+b;
+// }
+
+// // Fun call
+// sum(5,10)
+
+// IN TS
+let sums = (a: number, b: number): number => {
+    return a + b
+}
+console.log(sums(5, '10')); // valid cause we passed both number as an argument
+// sums(1, '2') // invalid cause we passed string as a second parameter
+
+
