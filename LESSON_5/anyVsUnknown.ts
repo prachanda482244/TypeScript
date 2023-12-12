445aea86d47012f69ab09a911ce125e38438c68d
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-12 22:39:38 +0545

    Lesson_5 learned about any and unknown and update lib in config

---
+++ LESSON_5/anyVsUnknown.ts
@@ -0,0 +1,37 @@
+// Type assertion in ts
+let anyType: any = "This is a string";
+let newNum: number = (anyType as string).length
+
+// The any type is the most flexible type in Typescript. It essentially turns off all type checking for the variables or expressions, you will see type any when you first write the codd
+
+// ANY
+let anyUser: any = 'user'
+anyUser = 10
+anyUser = true
+
+
+// Unknown
+let num2: unknown;
+num2 = 'string'
+num2 = true
+num2 = 10
+
+if (typeof num2 === 'number') {
+    console.log(num2 + 10);
+} else if (typeof num2 === 'boolean') {
+    console.log(true);
+}
+
+const fetchData = async (): Promise<unknown> => {
+    const response = await fetch('https://api.example.com/data');
+    const data = await response.json();
+    return data;
+};
+
+const processData = async () => {
+    const response = await fetchData();
+    if (typeof response === 'object') {
+        console.log(response);
+
+    }
+};
