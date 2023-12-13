c31961ca13d968c8d944fc7640a98bd937e99fc6
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2023-12-13 15:12:36 +0545

    LESSON_6 type inference in ts

---
+++ LESSON_6/practiceSet.ts
@@ -0,0 +1,9 @@
+// Declare a variable message and initialize it with the value  "Hello , Typescript". Infer the type of message using type inference.
+let msg = 'Hello, Typescript'
+console.log(msg);
+
+// Write a function calculateArea that takes the length and width parameter of type number and returns their product the return type of the function using type inference.
+const calculateArea = (length: number, width: number) => {
+    return length * width
+}
+console.log(calculateArea(2, 3));
---
+++ LESSON_6/typeInference.ts
@@ -0,0 +1,9 @@
+let typeString = "something" //The compiler infers the type string for the variable name
+
+let typeNumber = 120  //The compiler infers the type number for the variable name
+
+let typeBoolean = true  //The compiler infers the type boolean for the variable name
+
+
+
+
